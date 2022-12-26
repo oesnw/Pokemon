86cdd9db5f56deb757788dc0139d5e241912a4fd
oesnw <oesnw612@gmail.com>
2022-12-26 13:08:22 +0900

    test

---
+++ app/src/main/java/com/example/pokemon/PokemonActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokemonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon)
+
+
+    }
+}
